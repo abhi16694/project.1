8cfdd5c9554f1cc2fabdfbd90ef53d39c8a8e25d
Abhishek Tripathi <31342862+abhi16694@users.noreply.github.com>
2018-01-03 14:34:37 +0530

    Update passport.js

--- liveExam/server/middlewares/passport.js
+++ liveExam/server/middlewares/passport.js
@@ -14,6 +14,7 @@ exports.passportMiddleware = function(passport,app) {
     passport.use(new GoogleStrategy({
         clientID:     GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://liveexam.tk/auth/google/callback",
         callbackURL: "http://localhost:3000/auth/google/callback",
         passReqToCallback   : true
       },
@@ -26,6 +27,10 @@ exports.passportMiddleware = function(passport,app) {
     // setting up client id and client secret
 
     passport.use(new Strategy({
+        clientID: '401873783605095',
+        clientSecret: '93f8cef4c3bc1ac06fbeddf6568d4e38',
+        callbackURL: 'http://liveexam.tk/login/facebook/return',
+        /////////////////////////////////////// Localhost ////////////////////////////////////////
         clientID: '1977813652507076',
         clientSecret: '9c7b9d98afe44e136845ab6f4460ec39',
         callbackURL: 'http://localhost:3000/login/facebook/return',
